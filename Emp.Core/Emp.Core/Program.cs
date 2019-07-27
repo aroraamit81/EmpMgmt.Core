a06f1e5fd15a9c211ab27a2b6aa59312ee9ee680
Arora, Amit <aroraamit81@gmail.com>
2019-07-28 00:30:22 +1000

    mm

--- Emp.Core/Emp.Core/Program.cs
+++ Emp.Core/Emp.Core/Program.cs
@@ -15,6 +15,7 @@ namespace Emp.Core
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
